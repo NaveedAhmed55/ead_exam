3fbd0cb5351b349122eb37d352808b41d5e88c2e
Naveed Ahmed <76599103+NaveedAhmed55@users.noreply.github.com>
2023-05-27 15:47:17 +0500

    participation  component creation

---
+++ task_A/src/components/PollDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PollDisplay() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PollDisplay
---
+++ task_A/src/components/UseerParticipation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UseerParticipation() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UseerParticipation
